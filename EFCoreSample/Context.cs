e3b75f68a608719a5d9029447bc974aec55523dd
70_10 <7010.dev@gmail.com>
2021-05-27 23:06:32 +0900

    Add devcontainer

--- EFCoreSample/Context.cs
+++ EFCoreSample/Context.cs
@@ -7,6 +7,6 @@ namespace EFCoreSample
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer("Server=localhost;Database=SampleDB;User Id=SA;Password=Password123;");
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer("Server=db.local;Database=SampleDB;User Id=SA;Password=Password123;");
     }
 }
