f30c6e688b18c2f823e846bff029792fd0060a41
70_10 <7010.dev@gmail.com>
2021-05-27 15:10:20 +0900

    Initial commit

---
+++ EFCoreSample/Blog.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace EFCoreSample
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<Post> Posts { get; } = new List<Post>();
+    }
+}
---
+++ EFCoreSample/Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreSample
+{
+    public class Context : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer("Server=localhost;Database=SampleDB;User Id=SA;Password=Password123;");
+    }
+}
